08e36dce7539f0dd150b9884ada27a0c9cf1c634
thein moe win <colourfulmyanmar@gmail.com>
2023-12-19 23:24:25 +0630

    vue3game

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+ <h1>how fast can you catch me game</h1>
+ <button @click="start" :disabled="isplaying">play</button>
+<div v-if="isplaying">
+ <BlockBlue :delay="delay"/>
+</div>
+</template>
+
+<script>
+import BlockBlue from "./components/BlockBlue.vue"
+export default {
+  name: 'App',
+  components: {
+  BlockBlue
+  },
+  data(){
+      return{
+         isplaying:false,
+         delay:null
+      }
+  },
+  methods:{
+    start(){
+        this.isplaying=true;//play buttom function
+        this.delay=(2000+Math.random()*5000);//timer in blockblue
+        
+    }
+  }
+}
+</script>
+  
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BlockBlue.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="block" v-if="showblock" @click="stopTimer">click here{{ delay }}</div>
+</template>
+
+<script>
+export default {
+  props:['delay'],
+  data(){
+    return{
+      showblock:false,
+      scoreResult:0,
+      timer:null   
+    }
+  },
+  mounted(){
+    setTimeout(() => {
+      this.showblock=true;
+      this.startTimer()
+    },this.delay);
+  },
+  methods:{
+    startTimer(){
+        this.timer=setInterval(()=>{
+          this.scoreResult+=50;
+        },100)
+    },
+    stopTimer(){
+      clearInterval(this.timer);
+      console.log(this.scoreResult);
+    }
+  }
+  
+}
+</script>
+
+<style>
+  .block{
+    background-color: blueviolet;
+    width: 400px;
+    height: 200px;
+    margin: 20px auto;
+    padding-top: 30px;
+  }
+</style>
